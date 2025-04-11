f50cab3736ede4c60994109776d6b8454f52ffbe
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-04-12 02:56:54 +0600

    almost perfected

--- hypr/change_wallpapers.sh
+++ hypr/change_wallpapers.sh
@@ -5,7 +5,7 @@ WALLPAPER_DIRECTORY=/home/pixelatedstar/Pictures/Wallpapers
 WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
 
 hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "eDP-1,$WALLPAPER"
+hyprctl hyprpaper wallpaper ",$WALLPAPER"
 
 sleep 2
 
