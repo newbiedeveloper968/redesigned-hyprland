d89bf86c5ef903980e35f1f689b9a101eef47633
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2025-06-15 13:14:38 +0600

    Add debloat

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,48 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
-    
-alias c="clear"
-alias e="exit"
-alias vim='nvim'
-alias bl='bluetui'
-alias p='python3'
-alias vg='vim pyc/redesigned-hyprland/ultimate-guide.txt'
-source ~/pyc/learning-python/.venv/bin/activate.fish
-export HYPRSHOT_DIR="$HOME/Pictures/Screenshots"
+    # Disable default welcome message
+    function fish_greeting
+    end
 
-function fish_prompt
-    set_color c0c0c0
-    echo -n (prompt_pwd)
-    set_color ffffff
-    echo \n"➜ "
+    # Custom greeting
+    echo "🌱 Don't forget to use the Timer!"
+
+    # Customize syntax highlighting colors
+    set -g fish_color_command brcyan       # Commands
+    set -g fish_color_param white          # Arguments
+    set -g fish_color_error red            # Errors
+    set -g fish_color_quote green          # Strings in quotes
+    set -g fish_color_operator magenta     # |, >, &&, etc.
+    set -g fish_color_comment brblack      # Comments (#)
+    set -g fish_color_end brblue           # Statement terminators (;)
+    set -g fish_color_cwd yellow           # Current directory in prompt
+    set -g fish_color_user white
+    set -g fish_color_host gray
+    set -g fish_color_autosuggestion 555   # Ghost text
+    set -g fish_color_search_match --background=blue
+
+    # Aliases
+    alias c="clear"
+    alias e="exit"
+    alias vim='nvim'
+    alias bl='bluetui'
+    alias p='python3'
+
+    # Environment variables
+    set -x HYPRSHOT_DIR "$HOME/Pictures/Screenshots"
+
+    # Prompt customization
+    function fish_prompt
+        set_color c0c0c0
+        echo -n (prompt_pwd)
+        set_color ffffff
+        echo \n"➜ "
+    end
 end
+
+# === GTK dark theme override ===
+set -Ux GTK_THEME Adwaita:dark
+
+function pom
+    timer $argv
+    notify-send "Timer Done"
 end
